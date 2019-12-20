b6ffe84332fbe6895b25d4e931453e609ab3dabd
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-12-19 21:11:20 -0500

    DetailViewController outlets and labels wired up

--- RestaurantPassport/RestaurantPassport/RestaurantDetailViewController.swift
+++ RestaurantPassport/RestaurantPassport/RestaurantDetailViewController.swift
@@ -9,14 +9,44 @@
 import UIKit
 
 class RestaurantDetailViewController: UIViewController {
-
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var star1Button: UIButton!
+    @IBOutlet weak var star2Button: UIButton!
+    @IBOutlet weak var star3Button: UIButton!
+    @IBOutlet weak var star4Button: UIButton!
+    @IBOutlet weak var star5Button: UIButton!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var websiteTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Methods
+    // View
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    // Actions
+    @IBAction func star1Selected(_ sender: UIButton) {
+    }
+    @IBAction func star2Selected(_ sender: UIButton) {
+    }
+    @IBAction func star3Selected(_ sender: UIButton) {
+    }
+    @IBAction func star4Selected(_ sender: UIButton) {
+    }
+    @IBAction func star5Selected(_ sender: UIButton) {
+    }
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
     /*
     // MARK: - Navigation
 
