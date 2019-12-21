d7af62addda75b96b184bdff536ca41a1444e95d
Fabiola <saga.fabiola@gmail.com>
2019-12-21 01:07:37 -0600

    Added Token file for token response for User. Added signup and login functions for UserController

--- RestaurantPassport/RestaurantPassport/UserController.swift
+++ RestaurantPassport/RestaurantPassport/UserController.swift
@@ -7,3 +7,96 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
+
+class UserController {
+    
+    private let baseURL = URL(string: "https://restaurant-passport1.herokuapp.com/api/users")!
+    var token: TokenResponse?
+    
+    func signup(with user: User, completion: @escaping (Error?) -> Void) {
+        let signupURL = baseURL.appendingPathComponent("register")
+        
+        var request = URLRequest(url: signupURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding sign up object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
+    func login(with user: User, completion: @escaping (Error?) -> Void) {
+        let loginURL = baseURL.appendingPathComponent("login")
+        
+        var request = URLRequest(url: loginURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.token = try decoder.decode(TokenResponse.self, from: data)
+            } catch {
+                print("Error decoding token object: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }.resume()
+    }
+}
