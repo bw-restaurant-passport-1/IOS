c6553fae2201298e168a7d4053b1ea4d54dbdb5d
Fabiola <saga.fabiola@gmail.com>
2020-01-10 13:30:36 -0600

    Fixed loginButtonTapped IBAction on LoginViewController by reworking the initial if let and moving what belongs to sign up into the signup if statement, changed user variable to signupRequest to use SignUpRequest to pass the sign up form information.

--- RestaurantPassport/RestaurantPassport/LoginViewController.swift
+++ RestaurantPassport/RestaurantPassport/LoginViewController.swift
@@ -22,10 +22,15 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var fullNameTextField: UITextField!
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    // Need to connect to storyboard:
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
     
     // MARK: Properties
-    var userController: UserController?
+    var userController: UserController = UserController()
     var loginType = LoginType.signUp
+    var user: User?
     
     // MARK: - Methods
     // View
@@ -35,41 +40,45 @@ class LoginViewController: UIViewController {
     
     // Actions
     @IBAction func loginButtonTapped(_ sender: UIButton) {
-        guard let userController = userController else { return }
         
         if let username = usernameTextField.text,
             !username.isEmpty,
             let password = passwordTextField.text,
-            !password.isEmpty,
-            let name = fullNameTextField.text,
-            !name.isEmpty,
-            let city = cityTextField.text,
-            !city.isEmpty,
-            let email = emailTextField.text,
-            !email.isEmpty {
+            !password.isEmpty {
             
-            let user = User(username: username, password: password, name: name, email: email, city: city)
+            
             
             if loginType == .signUp {
-                userController.signup(with: user) { error in
-                    if let error = error {
-                        print("Error occured during sign up: \(error)")
-                    } else {
-                        DispatchQueue.main.async {
-                            let alertController = UIAlertController(title: "Sign up successful!", message: "Now please log in.", preferredStyle: .alert)
-                            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                            alertController.addAction(alertAction)
-                            self.present(alertController, animated: true, completion:  {
-                                self.loginSegmentedControl.selectedSegmentIndex = 1
-                                self.loginType = .login
-                                self.passwordTextField.text = nil
-                                self.loginButton.setTitle("Log in", for: .normal)
-                            })
+                if let name = fullNameTextField.text,
+                    !name.isEmpty,
+                    let city = cityTextField.text,
+                    !city.isEmpty,
+                    let email = emailTextField.text,
+                    !email.isEmpty {
+                    
+                    let signupRequest = SignUpRequest(username: username, password: password, name: name, email: email, city: city)
+                    userController.signup(type: .newUser, with: signupRequest) { error in
+                        if let error = error {
+                            print("Error occured during sign up: \(error)")
+                        } else {
+                            DispatchQueue.main.async {
+                                let alertController = UIAlertController(title: "Sign up successful!", message: "Now please log in.", preferredStyle: .alert)
+                                let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                                alertController.addAction(alertAction)
+                                self.present(alertController, animated: true, completion:  {
+                                    self.loginSegmentedControl.selectedSegmentIndex = 0
+                                    self.loginType = .login
+                                    self.passwordTextField.text = nil
+                                    self.fullNameTextField.isHidden = true
+                                    self.emailTextField.isHidden = true
+                                    self.cityTextField.isHidden = true
+                                })
+                            }
                         }
                     }
                 }
-            } else {
-                userController.login(with: user) { error in
+            } else if loginType == .login {
+                userController.login(type: .existingUser, withUsername: username, withPassword: password) { error in
                     if let error = error {
                         print("Error occured during log in: \(error)")
                     } else {
@@ -79,19 +88,38 @@ class LoginViewController: UIViewController {
                     }
                 }
             }
+        } else {
+            self.dismiss(animated: true, completion: nil)
         }
     }
     
-    //Todo: Need to connect to storyboard
+    //Need to connect to storyboard:
     @IBAction func loginTypeChanged(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
-            loginType = .signUp
-            passwordTextField.text = nil
-            loginButton.setTitle("Sign Up", for: .normal)
-        } else {
             loginType = .login
+            usernameTextField.text = nil
             passwordTextField.text = nil
-            loginButton.setTitle("Log in", for: .normal)
+            fullNameTextField.isHidden = true
+            emailTextField.isHidden = true
+            cityTextField.isHidden = true
+            nameLabel.isHidden = true
+            cityLabel.isHidden = true
+            emailLabel.isHidden = true
+            self.loginButton.setTitle("Log In", for: .normal)
+        } else if sender.selectedSegmentIndex == 1 {
+            loginType = .signUp
+            usernameTextField.text = nil
+            passwordTextField.text = nil
+            fullNameTextField.text = nil
+            emailTextField.text = nil
+            cityTextField.text = nil
+            fullNameTextField.isHidden = false
+            emailTextField.isHidden = false
+            cityTextField.isHidden = false
+            nameLabel.isHidden = false
+            cityLabel.isHidden = false
+            emailLabel.isHidden = false
+            self.loginButton.setTitle("Sign Up", for: .normal)
         }
     }
 
