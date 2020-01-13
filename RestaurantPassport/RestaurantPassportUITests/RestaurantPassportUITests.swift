5db746f9a1739eb44889ee56d0fccffb15d260bf
Fabiola <saga.fabiola@gmail.com>
2020-01-13 11:42:25 -0600

    Added Unit Test and UI Test files

---
+++ RestaurantPassport/RestaurantPassportUITests/RestaurantPassportUITests.swift
@@ -0,0 +1,19 @@
+//
+//  RestaurantPassportUITests.swift
+//  RestaurantPassportUITests
+//
+//  Created by Fabiola S on 1/13/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import XCTest
+
+class RestaurantPassportUITests: XCTestCase {
+
+    var app: XCUIApplication {
+        return XCUIApplication()
+    }
+    
+    
+    
+}
